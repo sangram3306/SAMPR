f1e45797e3f6ec69629e2e561b04d80231d83f04
Sangram <sangramm954@gmail.com>
2018-09-04 12:21:54 +0530

    #modified again

--- Test.java
+++ Test.java
@@ -2,5 +2,9 @@ class MyClass extends Parent implements Interface {
 
   public static void main(String[] args) {
     System.out.println("hello");
+    if (true) {
+      System.out.println("awesome");
+    }
+
   }
 }
